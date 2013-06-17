027dd42be56740c2abc0a859f70f33870460e096
D4rKDeagle <filipefreitas_8@hotmail.com>
2013-06-17 21:12:32 +0100

    Protect the mod under the terms of GNU General Public License V3.0

--- hygienic_common/us/joaogldarkdeagle/hygienic/Hygienic.java
+++ hygienic_common/us/joaogldarkdeagle/hygienic/Hygienic.java
@@ -1,3 +1,20 @@
+/**
+ * This file is part of Hygienic.
+
+    Hygienic is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Hygienic is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Hygienic.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package us.joaogldarkdeagle.hygienic;
 
 import net.minecraft.block.Block;
@@ -21,11 +38,8 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
-import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
-import cpw.mods.fml.common.registry.TickRegistry;
-import cpw.mods.fml.relauncher.Side;
 
 @Mod(modid = ModInfo.MOD_ID, name = ModInfo.MOD_NAME, version = ModInfo.MOD_VERSION)
 @NetworkMod(clientSideRequired = true, serverSideRequired = true)

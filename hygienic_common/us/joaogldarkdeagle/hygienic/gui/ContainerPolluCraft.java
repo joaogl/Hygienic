1ce64a27219a8375d00e9102c03909ad84de92a6
joaogl <joaoguerralourenco@hotmail.com>
2013-06-18 16:35:51 +0100

    PolluCraft almost finished.

--- hygienic_common/us/joaogldarkdeagle/hygienic/gui/ContainerPolluCraft.java
+++ hygienic_common/us/joaogldarkdeagle/hygienic/gui/ContainerPolluCraft.java
@@ -42,14 +42,16 @@ public class ContainerPolluCraft extends Container {
         this.posY = par4;
         this.posZ = par5;
 
-        this.addSlotToContainer(new SlotPolluCraft(par1InventoryPlayer.player, this.craftMatrix, this.craftResult, 0, 124, 35));
-        this.addSlotToContainer(new SlotPolluCraft(par1InventoryPlayer.player, this.craftMatrix, this.craftResult, 0, 150, 35));
+        this.addSlotToContainer(new SlotPolluCraft(par1InventoryPlayer.player, this.craftMatrix, this.craftResult, 0, 121, 33));
+        this.addSlotToContainer(new SlotPolluCraft(par1InventoryPlayer.player, this.craftMatrix, this.craftResult, 0, 149, 33));
         int var6;
         int var7;
 
+        // var7 + var6 * 5= no Idea... but it crash the game so dont change
+        // it!;
         for (var6 = 0; var6 < 4; ++var6)
             for (var7 = 0; var7 < 4; ++var7)
-                this.addSlotToContainer(new Slot(this.craftMatrix, var7 + var6 * 3, 30 + var7 * 18, 17 + var6 * 18));
+                this.addSlotToContainer(new Slot(this.craftMatrix, var7 + var6 * 3, 8 + var7 * 18, 6 + var6 * 18));
 
         // var7 + var6 * 9 + 2 = no Idea... but it crash the game so dont change
         // it!; 8 + var7 * 18 = size of it; 100 + var6 * 18 = y

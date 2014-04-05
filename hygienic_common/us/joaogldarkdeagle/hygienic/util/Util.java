6c89b3897f1effdb33fd66c84e4463fef8ad9cce
joaogl <joaoguerralourenco@hotmail.com>
2014-04-05 14:22:57 +0100

    Util cleared out.

--- hygienic_common/us/joaogldarkdeagle/hygienic/util/Util.java
+++ hygienic_common/us/joaogldarkdeagle/hygienic/util/Util.java
@@ -1,12 +1,7 @@
 package us.joaogldarkdeagle.hygienic.util;
 
 import net.minecraft.util.ChatComponentStyle;
-import net.minecraft.util.ChatComponentText;
 
 public class Util {
 
-	/*
-	public static ChatComponentStyle buildChatMessageComponent(String text) {
-		return ChatComponent.createFromJson("{ text: '" + text + "' }");
-	}*/
 }
